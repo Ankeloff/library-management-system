8216710d91d3d11274fd2ee8fc6bdaf2deebf28f
Ankeloff <ankeloff1@gmail.com>
2022-08-05 16:29:01 +0500

    initial commit

---
+++ src/main/java/uz/pdp/librarymanagementsystem/utils/Util.java
@@ -0,0 +1,7 @@
+package uz.pdp.librarymanagementsystem.utils;
+
+public class Util {
+
+//    public final static String UPLOAD_DIRECTORY = "C:\\upload\\";
+    public final static String UPLOAD_DIRECTORY = "C:\\modul - 6\\library-management-system\\src\\main\\webapp\\images";
+}
